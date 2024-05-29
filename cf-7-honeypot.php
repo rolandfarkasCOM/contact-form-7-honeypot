60324584934ffbf870d83f6d263be56045673416
Roland Farkas <hello@rolandfarkas.com>
2024-05-29 19:14:15 +0100

    Updated to v1.0.1

--- cf-7-honeypot.php
+++ cf-7-honeypot.php
@@ -2,14 +2,14 @@
 /**
  * Plugin Name: Simple Contact Form 7 Honeypot
  * Description: Adds a hidden field with a random name to Contact Form 7 forms to block bot/spam submissions.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Roland Farkas
  * Author URI: https://rolandfarkas.com
  * License: GPL3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: cf-7-honeypot
  * Requires at least: 4.9
- * Tested up to: 6.5.3
+ * Tested up to: 6.5
  */
 
 // Exit if accessed directly
@@ -17,7 +17,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-// Hook into the 'wpcf7_form_hidden_fields' filter to add the hidden honeypot field
+// Hook into the 'wpcf7_form_hidden_fields' filter to add the hidden honeypot field and nonce field
 add_filter('wpcf7_form_hidden_fields', 'add_random_honeypot_hidden_field');
 
 function add_random_honeypot_hidden_field($hidden_fields) {
@@ -26,21 +26,29 @@ function add_random_honeypot_hidden_field($hidden_fields) {
 
     // Add the random field name to the hidden fields array
     $hidden_fields['honeypot_field_name'] = $field_name;
-	$hidden_fields[$field_name] = '';
+    $hidden_fields[$field_name] = '';
+
+    // Generate a nonce and add it to the hidden fields array
+    $nonce = wp_create_nonce('cf7_honeypot_nonce');
+    $hidden_fields['cf7_honeypot_nonce'] = $nonce;
 
     return $hidden_fields;
 }
 
-// Hook into the 'wpcf7_validate' filter to check the honeypot field on form submission
+// Hook into the 'wpcf7_validate' filter to check the honeypot field and nonce on form submission
 add_filter('wpcf7_validate', 'check_random_honeypot_field', 10, 2);
 
 function check_random_honeypot_field($result, $tags) {
     // Retrieve the field name from the hidden input
     $field_name = sanitize_text_field($_POST['honeypot_field_name']);
 
+    // Verify nonce
+    if (!isset($_POST['cf7_honeypot_nonce']) || !wp_verify_nonce($_POST['cf7_honeypot_nonce'], 'cf7_honeypot_nonce')) {
+        $result->invalidate('', __('Nonce verification failed.', 'cf-7-honeypot'));
+    }
+
     // Check the honeypot field value
     if ($field_name && isset($_POST[$field_name]) && !empty($_POST[$field_name])) {
-		error_log($_POST[$field_name]);
         $result->invalidate('', __('Spam detected.', 'cf-7-honeypot'));
     }
 
--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === Simple Contact Form 7 Honeypot ===
 Contributors: rolandfarkas
-Tags: cf7, contact form 7, contactform7, honepot, bots, bot, spam, spam protection, spam emails, contact form
+Tags: contact form 7, honepot, bot, spam protection, spam
 Requires at least: 4.9
-Tested up to: 6.5.3
-Stable tag: 1.0.0
+Tested up to: 6.5
+Stable tag: 1.0.1
 License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Requires PHP: 5.6
@@ -34,3 +34,5 @@ Simply deactivate and delete the plugin.
 == Changelog ==
 = 1.0.0 =
 * Plugin released.
+= 1.0.1 =
+* Tweaks
